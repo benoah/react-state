f728034781b9f17f547f7b833f8308f36a300388
ben <noahbenaas@gmail.com>
2021-03-01 17:18:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      answer: "Yes it is  ",
-      name: "Ben ",
-      age: "40",
+      isLoggedIn: false,
     };
   }
 
   render() {
+    let wordDisplay;
+    if (this.state.isLoggedIn) {
+      wordDisplay = "in";
+    } else {
+      wordDisplay = "out";
+    }
     return (
       <div>
-        <h1>Is state important to know? {this.state.answer}</h1>
-        <h1>{this.state.name}</h1>
-        <h3>{this.state.age} years old</h3>
+        <h1>You are currently logged {wordDisplay}</h1>
       </div>
     );
   }
